3ef0846d0cb63af6670251770972effaf950b673
comu2e <comu2e@gmail.com>
2022-02-20 01:20:29 +0900

    fix:Next,jsからswaggerにアクセス

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ build:
 	docker-compose build
 	docker-compose exec backend realise start
 
+restart:
+	docker-compose down
+	docker-compose build
+	docker-compose up -d
--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -3,17 +3,28 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home: NextPage = (props) => {
   return (
 
     <div>
+      {props.bpi.time.updated}
+
+      {props.user[0].name}
 
     </div>
   )
 }
 
-export default Home(){
-  return (async function () {
-    let todoUrl = "htt"
-  })
+Home.getInitialProps = async function () {
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+  const data = await res.json();
+  const res2 = await fetch('http://swagger-api:4010/users')
+  const data2 = await res2.json();
+  return {
+    bpi: data,
+    user: data2
+  }
+
 }
+
+export default Home;
