e53b34833b8ff7d6ae86bc9e8012ba53dbcfd3bd
comu2e <comu2e@gmail.com>
2022-02-20 23:39:30 +0900

    add:sass対応,特定のデザイン適用できるようにする

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Layout from "../src/components/_layouts/Layout"
 import Todos from "../src/components/todo"
 import { ReactChild, ReactFragment, ReactPortal } from 'react'
+import styles from '../styles/about.module.scss'  // scssをインポートする
 
 export default function Home() {
 
   return (
     <>
+      <div className={styles.article__title}>Hello</div>
       <Layout title="Layout Test"> </Layout>
       <Todos> </Todos>
     </>
